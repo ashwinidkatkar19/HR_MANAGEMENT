aa1734772aec2dd8be1a09274bf03a4f3bc1b965
Ashwini Dnyaneshwar Katkar <ashwinidkatkar19@gmail.com>
2024-12-08 23:39:37 +0530

    Update views.py

--- DepartmentApp/views.py
+++ DepartmentApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from DepartmentApp.models import Department
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 def home(request):
@@ -28,28 +29,36 @@ def Deletedepartment(request, dept_id):
 
 # def updateDepartment(request, did):
 #     if request.method == "GET":
-#         d = Department.objects.filter(dept_id=did) 
+#         d = Department.objects.filter(dept_id=did)
 #         context = {}
-#         context['department'] = d #in case if we use get
+#         context['department'] = d
 #         return render(request,'updatedepartment.html',context)
 #     else:
 #         dep = Department.objects.filter(dept_id = did)
-#         # d is the queryset, on Queryset, we can call update and delete
 #         n = request.POST['dept_name']
 #         d = request.POST['description']
 #         dep.update(dept_name=n, description =d)
 #         return redirect('/')
 
-def update_department(request, department_id):
-    department = get_object_or_404(Department, id=department_id)
-    
-    if request.method == 'POST':
-        department.dept_name = request.POST.get('dept_name')
-        department.description = request.POST.get('description')
-        department.save()
-        
-    return render(request, 'update_department.html', {'department': department})
+def updateDepartment(request, did):
+    try:
+        # Fetch the department by ID or raise a 404 error if not found
+        department = Department.objects.get(dept_id=did)
+    except Department.DoesNotExist:
+        return HttpResponse("Department not found", status=404)
 
+    if request.method == "GET":
+        # Render the form with the existing department details
+        return render(request, 'updatedepartment.html', {'department': department})
+
+    elif request.method == "POST":
+        # Get updated values from the POST request
+        department.dept_name = request.POST.get('dept_name', department.dept_name)
+        department.description = request.POST.get('description', department.description)
+        # Save the changes to the database
+        department.save()
+        # Redirect to the home page after updating
+        return redirect('/')
 
 
 
