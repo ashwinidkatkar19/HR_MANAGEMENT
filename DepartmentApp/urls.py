c5fc268d382b0af26b5805e7b90f9354b9ac802f
Ashwini Dnyaneshwar Katkar <ashwinidkatkar19@gmail.com>
2024-12-08 23:38:33 +0530

    Update urls.py

--- DepartmentApp/urls.py
+++ DepartmentApp/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from DepartmentApp import views
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home),
+    # path('department/update/<int:department_id>/', views.update_department, name='update_department'),
     path('createdepartment/',views.createDepartment),
     path('delete/<int:dept_id>',views.Deletedepartment),
     path('edit/<int:did>',views.updateDepartment)
 
 ]
+
 
 
 
